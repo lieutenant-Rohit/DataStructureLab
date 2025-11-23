0b5a73757be291ae0e4d5e688e8a72a556b6f02f
Rohit <rohitbishtt69@gmail.com>
2025-11-24 04:40:26 +0530

    DSA LAB

---
+++ Searching/src/Floor.java
@@ -0,0 +1,28 @@
+public class Floor {
+
+    static int floor(int[] arr , int target)
+    {
+        int start = 0;
+        int end = arr.length-1;
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            if(arr[mid] == target)
+            {
+                return mid;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return arr[end];
+    }
+    public static void main(String[] args) {
+        int[] arr = { 2,3,5,9,14,16,18};
+        int target = 15;
+        System.out.println("Ceiling of the number "+target+ " is:- "+floor(arr,target));
+    }
+}
